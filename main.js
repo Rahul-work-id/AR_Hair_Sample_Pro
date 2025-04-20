cf736afb5731682cf4d9799983db6142a861fbac
Rahul-work-id <rahulkumarworkid@gmail.com>
2025-04-20 11:28:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ function init_threeScene(spec) {
 
     // Optional: scale and position if needed
     model.scale.set(.8, .8, .8);
-    model.position.set(0, 0.6, 0);
+    model.position.set(0, 0.5, -0.2);
 
     // Add the model to the glasses object or directly to faceObject
     threeStuffs.faceObject.add(model);
